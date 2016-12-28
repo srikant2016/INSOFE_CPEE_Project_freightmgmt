25aae4d59493559cd5cce211f975d7a664459c60
srikant2016 <atmakuri.srikant@gmail.com>
2016-12-28 08:45:54 -0800

    Preprocessing - in progress

--- FM_AtmakuriSrikant.R
+++ FM_AtmakuriSrikant.R
@@ -10,6 +10,10 @@ regionlookups <- read.csv("regionlookups.csv",header=T, na.strings = c("","NA","
 marketzips <- read.csv("marketzips.csv",header=T, na.strings = c("","NA"," "))
 equipmentcodes <- read.csv("equipmentcodes.csv",header=T, na.strings = c("","NA"," "))
 
+library(dplyr)
+library(lubridate)
+library(plyr)
+
 #View(spotfreight)
 # View(regionlookups)
 # View(marketzips)
@@ -35,17 +39,15 @@ summary(complete.cases(spotfreight)) # checks if any of the rows has NAs in any 
 
 
 cat("Total orders count : " , length(spotfreight$ORDER_NBR))  # 34693
-cat("Unique orders count : " , length(unique(spotfreight$ORDER_NBR))) # 23512
+cat("Unique orders count : " , n_distinct(spotfreight$ORDER_NBR)) # 23512
 
-# There is multiple rows for the same data
-
+# There are multiple rows for the same data (like order_number with slight difference in zip, appt, etc...). Below are such samples:
 #"ORDER_NBR","EQUIPMENT_TYPE","CUSTOMER_MILES","WEIGHT","ORDER_COST","FIRST_PICK_ZIP","FIRST_PICK_EARLY_APPT","FIRST_PICK_LATE_APPT","LAST_DELIVERY_ZIP","LAST_DELIVERY_EARLY_APPT","LAST_DELIVERY_LATE_APPT","IS_HAZARDOUS","CREATED_DATE"
 # 167555,"V",2509,12982,0,"47805","1/22/2016 0:00","1/22/2016 0:00","97321","1/25/2016 0:00","1/25/2016 0:00","N","1/5/2016 0:00"
 # 167555,"V",2509,12982,0,"47805","1/22/2016 0:00","1/22/2016 0:00","97218","1/26/2016 0:00","1/26/2016 0:00","N","1/5/2016 0:00"
 # 167555,"V",2509,12982,0,"47805","1/22/2016 0:00","1/22/2016 0:00","97218","1/25/2016 0:00","1/26/2016 0:00","N","1/5/2016 0:00"
 # 167555,"V",2509,12982,0,"47805","1/22/2016 0:00","1/22/2016 0:00","98390","1/26/2016 0:00","1/26/2016 0:00","N","1/5/2016 0:00"
 # 167555,"V",2509,12982,0,"47805","1/22/2016 0:00","1/22/2016 0:00","V3V4G1","1/26/2016 0:00","1/26/2016 0:00","N","1/5/2016 0:00"
-
 # 169459,"V",716,9420,1500,"45011","1/21/2016 0:00","1/21/2016 0:00","47909","1/21/2016 0:00","1/22/2016 0:00","N","1/16/2016 0:00"
 # 169459,"V",716,9420,1500,"47909","1/21/2016 0:00","1/22/2016 0:00","47909","1/21/2016 0:00","1/22/2016 0:00","N","1/16/2016 0:00"
 # 169459,"V",716,9420,1500,"40330","1/21/2016 0:00","1/21/2016 0:00","47909","1/21/2016 0:00","1/22/2016 0:00","N","1/16/2016 0:00"
@@ -59,20 +61,52 @@ cat("Unique orders count : " , length(unique(spotfreight$ORDER_NBR))) # 23512
 # remove duplicate records (ignoring the "CREATED_DATE" field)
 spotfreight_unq <- spotfreight[!duplicated(spotfreight[,!(colnames(spotfreight) %in% c("CREATED_DATE"))]), ]
 
+# removes space and - characters from both FIRST_PICK_ZIP & LAST_DELIVERY_ZIP columns
+spotfreight$FIRST_PICK_ZIP <- gsub("[ -]", "", spotfreight$FIRST_PICK_ZIP) 
+spotfreight$LAST_DELIVERY_ZIP <- gsub("[ -]", "", spotfreight$LAST_DELIVERY_ZIP)
+
 # remove records with alphanumeric zip codes either in FIRST_PICK_ZIP or LAST_DELIVERY_ZIP or both
-spotfreight$FIRST_PICK_ZIP <- gsub("[ -]", "", spotfreight$FIRST_PICK_ZIP) # removes space and - characters from FIRST_PICK_ZIP
-spotfreight$LAST_DELIVERY_ZIP <- gsub("[ -]", "", spotfreight$LAST_DELIVERY_ZIP) # removes space and - characters from LAST_DELIVERY_ZIP
-
 check.numeric <- function(N){ !length(grep("[^[:digit:]]", as.character(N)))}
+spotfreight <- spotfreight[sapply(spotfreight$FIRST_PICK_ZIP, check.numeric) & sapply(spotfreight$LAST_DELIVERY_ZIP, check.numeric),] 
+#spotfreight$VALID_PICK_ZIP <- sapply(spotfreight$FIRST_PICK_ZIP, check.numeric)
+#spotfreight$VALID_PICK_ZIP <- NULL
+#spotfreight$VALID_DELIVERY_ZIP <- sapply(spotfreight$LAST_DELIVERY_ZIP, check.numeric)
+#spotfreight$VALID_DELIVERY_ZIP <- NULL
 
-spotfreight$VALID_DELIVERY_ZIP <- sapply(spotfreight$LAST_DELIVERY_ZIP, check.numeric)
-spotfreight$VALID_DELIVERY_ZIP <- sapply(spotfreight$LAST_DELIVERY_ZIP, check.numeric)
+# check & remove records with zero values in ORDER_COST field (the target variable)
+summary(spotfreight$ORDER_COST)
+arrange(summarise(group_by(spotfreight, ORDER_COST), orders = n()), desc(-ORDER_COST)) # there are around 6326 records with zero ORDER_COST (the target variable)
+n_distinct(spotfreight) # 34273
+spotfreight <- spotfreight[!((spotfreight$ORDER_COST == 0) | (spotfreight$ORDER_COST == 0.01)),] 
+n_distinct(spotfreight) # 27946 (6327 records with  0 ORDER_COST excluded )
 
-as.numeric(spotfreight$FIRST_PICK_ZIP)
+# check & remove records with zero or negative WEIGHT
+summary(spotfreight$WEIGHT)
+arrange(summarise(group_by(spotfreight, WEIGHT), orders = n()), desc(-WEIGHT)) # there are around 1681 records with zero WEIGHT (to be excluded)
+spotfreight <- spotfreight[!(spotfreight$WEIGHT == 0),] 
+n_distinct(spotfreight) # 26265 (1681 records with 0 WEIGHT excluded )
+
+# check & remove records with zero or negative CUSTOMER_MILES
+summary(spotfreight$CUSTOMER_MILES)
+arrange(summarise(group_by(spotfreight, CUSTOMER_MILES), orders = n()), desc(-CUSTOMER_MILES)) # there are around 1681 records with zero CUSTOMER_MILES (to be excluded)
+spotfreight <- spotfreight[!(spotfreight$CUSTOMER_MILES == 0),] 
+n_distinct(spotfreight) # 26166 (99 records with 0 CUSTOMER_MILES excluded )
+
+# check & understand equipments types
+summary(spotfreight$EQUIPMENT_TYPE)
+equipments <- arrange(summarise(group_by(spotfreight, EQUIPMENT_TYPE), orders = n()), desc(orders))
+n_distinct()
+
+# check & remove records with zero or negative CUSTOMER_MILES
+summary(spotfreight$CUSTOMER_MILES)
+arrange(summarise(group_by(spotfreight, CUSTOMER_MILES), orders = n()), desc(-CUSTOMER_MILES)) # there are around 1681 records with zero CUSTOMER_MILES (to be excluded)
+spotfreight <- spotfreight[!(spotfreight$CUSTOMER_MILES == 0),] 
+n_distinct(spotfreight) # 26166 (99 records with 0 CUSTOMER_MILES excluded )
+
+
 which(is.na(as.numeric(as.character(spotfreight$FIRST_PICK_ZIP))))
 sum(is.na(as.numeric(as.character(spotfreight$FIRST_PICK_ZIP))))
 sapply(spotfreight$FIRST_PICK_ZIP, length(gsub("[:digit:]", "", unique(spotfreight$FIRST_PICK_ZIP))
-sapply()
 gsub("\\D+", "", "V2S7W6",ignore.case = TRUE)
 gsub("[:digit:]", "", "67119",ignore.case = TRUE)
 gsub("[ -]", "", " N5W 6C7")
@@ -82,15 +116,15 @@ spotfreight$FIRST_PICK_ZIP<-gsub("[\\S+-]", "", spotfreight$FIRST_PICK_ZIP)
 !is.na(as.numeric(spotfreight$FIRST_PICK_ZIP))
 check.numeric <- function(N){ !length(grep("[^[:digit:]]", as.character(N)))}
 check.numeric("0")
-# check & remove records with NA or zero values in ORDER_COST field (the target variable)
 
-# check & remove records with zero or negative WEIGHT 
+
+ 
 
 # 
 
 
 
-library(plyr)
+
 dfOrders <- as.data.frame(count(spotfreight$ORDER_NBR)) # creates a data frame with order number and corresponding frequency (repeation count)
 #colnames(dfOrders)
 dfOrders <- arrange(dfOrders, desc(freq)) # sorts the newly created data frame in descending order of the frequency column
@@ -126,7 +160,7 @@ arrange(count(dfOrders, "freq"), desc(freq.1))
 #spotfreight_cor
 #corrplot(spotfreight_cor)
 
-library(lubridate)
+
 #zz <- as.data.frame(cbind(year = year(px), month = month(px), day = day(px)))
 #sum(is.na(zz))
 
@@ -136,13 +170,19 @@ library(lubridate)
 #head(px1)
 #head(spotfreight$FIRST_PICK_EARLY_APPT)
 
-colnames(spotfreight)
+
 # splitting the date&time cols to day,month,year cols
-px1 <- as.POSIXlt(spotfreight$FIRST_PICK_EARLY_APPT, format="%m/%d/%Y %H:%M", tz="GMT")
-px2 <- as.POSIXlt(spotfreight$FIRST_PICK_LATE_APPT, format="%m/%d/%Y %H:%M", tz="GMT")
-px3 <- as.POSIXlt(spotfreight$LAST_DELIVERY_EARLY_APPT, format="%m/%d/%Y %H:%M", tz="GMT")
-px4 <- as.POSIXlt(spotfreight$LAST_DELIVERY_LATE_APPT, format="%m/%d/%Y %H:%M", tz="GMT")
-px5 <- as.POSIXlt(spotfreight$CREATED_DATE, format="%m/%d/%Y %H:%M", tz="GMT")
+# reference: http://www.cyclismo.org/tutorial/R/time.html
+# 2 different types specific to time data fields in R. the POSIXct and POSIXlt 
+# POSIXct : this data type is the number of seconds since the start of January 1, 1970. Negative numbers represent the number of seconds before this time, and positive numbers represent the number of seconds afterwards
+# POSIXlt : this data type is a vector, and the entries in the vector have different meanings for day, month, year, week, etc... of that date
+# strftime : this function is used to take a time data type and convert it to a string. 
+
+spotfreight$FIRST_PICK_EARLY_APPT <- as.POSIXlt(spotfreight$FIRST_PICK_EARLY_APPT, format="%m/%d/%Y %H:%M", tz="GMT")
+spotfreight$FIRST_PICK_LATE_APPT <- as.POSIXlt(spotfreight$FIRST_PICK_LATE_APPT, format="%m/%d/%Y %H:%M", tz="GMT")
+spotfreight$LAST_DELIVERY_EARLY_APPT <- as.POSIXlt(spotfreight$LAST_DELIVERY_EARLY_APPT, format="%m/%d/%Y %H:%M", tz="GMT")
+spotfreight$LAST_DELIVERY_LATE_APPT <- as.POSIXlt(spotfreight$LAST_DELIVERY_LATE_APPT, format="%m/%d/%Y %H:%M", tz="GMT")
+spotfreight$CREATED_DATE <- as.POSIXlt(spotfreight$CREATED_DATE, format="%m/%d/%Y %H:%M", tz="GMT")
 
 spotfreight1 <- spotfreight[, !(colnames(spotfreight) %in% c("FIRST_PICK_EARLY_APPT","FIRST_PICK_LATE_APPT","LAST_DELIVERY_EARLY_APPT", "LAST_DELIVERY_LATE_APPT", "CREATED_DATE"))]
 colnames(spotfreight1)
