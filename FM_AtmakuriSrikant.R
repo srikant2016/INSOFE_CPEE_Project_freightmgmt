9fb984611ee7a8074aadafedc80e71f1bf3c38a5
srikant2016 <atmakuri.srikant@gmail.com>
2016-12-29 09:54:04 -0800

    Model built - base cut

--- FM_AtmakuriSrikant.R
+++ FM_AtmakuriSrikant.R
@@ -16,6 +16,7 @@ equipmentcodes <- read.csv("equipmentcodes.csv",header=T, na.strings = c("","NA"
 library(plyr)
 library(dplyr)
 library(lubridate)
+library(stringi)
 
 # View(spotfreight)
 # View(regionlookups)
@@ -96,7 +97,6 @@ arrange(plyr::count(dfOrders, "freq"), desc(freq.1))
 # 14   16     16
 # 15   15     15
 # 16   14     14
-
 ########## remove duplicate records (ignoring the "CREATED_DATE" field) ##########
 spotfreight_unq <- spotfreight[!duplicated(spotfreight[,!(colnames(spotfreight) %in% c("CREATED_DATE"))]), ]
 
@@ -132,7 +132,7 @@ spotfreight <- spotfreight[!(spotfreight$CUSTOMER_MILES == 0),]
 n_distinct(spotfreight) # 26166 (99 records with 0 CUSTOMER_MILES excluded )
 
 ########## check & understand equipments types ##########
-#and add 2 new columns based on the values in equipmentcodes dataframe and equipment_type value in spotfreight data frame
+########## and add 2 new columns based on the values in equipmentcodes dataframe and equipment_type value in spotfreight data frame ########## 
 summary(spotfreight$EQUIPMENT_TYPE) # this is to understand how many different types of equipments are avaialable in the given raw  data
 equipments <- arrange(summarise(group_by(spotfreight, EQUIPMENT_TYPE), orders = n()), desc(orders)) # this is to understand how orders with what equipment type code in the raw data
 spotfreight$Equipment_New_Abbr <- equipmentcodes[match(spotfreight$EQUIPMENT_TYPE, equipmentcodes$EQUIPMENT_TYPE),2]
@@ -146,21 +146,44 @@ spotfreight <- na.omit(spotfreight) # 95records removed for which EQUIPMENT_ABBR
 summary(complete.cases(spotfreight)) # 26071 obs.
 
 
+########## check & understand markets ##########
+########## and add 2 new columns FIRST_PICK_MARKET and LAST_DELIVERY_MARKET based on the respective zip codes and the ##########
+########## corresponding market values from marketzips dataframe ##########
+#summary(marketzips)
+#str(marketzips)
+#FIRST_PICK_ZIPS <- arrange(summarise(group_by(spotfreight, FIRST_PICK_ZIP), orders = n()), -desc(FIRST_PICK_ZIP))
+#LAST_DELIVERY_ZIPS <- arrange(summarise(group_by(spotfreight, LAST_DELIVERY_ZIP), orders = n()), -desc(LAST_DELIVERY_ZIP))
+get3digits <- function(z){x="0"; z=stri_trim_both(z); if(!is.na(z) && !stri_isempty(z) && (stri_length(z)>2)) { s=3; l=stri_length(z); if(l==3) s=1 else if(l==4) s=2 else s=3; x=stri_sub(z,0,s); }; return(x);}
+#get3digits("01013")
+#match("010", marketzips$Zips)
+spotfreight$FIRST_PICK_MARKET <- marketzips[match(sapply(spotfreight$FIRST_PICK_ZIP, get3digits), marketzips$Zips),2]
+summary(spotfreight$FIRST_PICK_MARKET)
+spotfreight$LAST_DELIVERY_MARKET <- marketzips[match(sapply(spotfreight$LAST_DELIVERY_ZIP, get3digits), marketzips$Zips),2]
+summary(spotfreight$LAST_DELIVERY_MARKET)
+spotfreight_Missing_MARKET_ZIPS <- spotfreight[is.na(spotfreight$FIRST_PICK_MARKET) | is.na(spotfreight$LAST_DELIVERY_MARKET),] #1958 obss.
+#spotfreight <- na.omit(spotfreight) 
+#summary(complete.cases(spotfreight)) # 26071 obs.
+
+########## check & understand regions ##########
+########## and add 2 new columns FIRST_PICK_REGION and LAST_DELIVERY_REGION based on the respective zip codes and the ##########
+########## corresponding market values from regionlookups dataframe ##########
+summary(regionlookups)
+#str(regionlookups)
+#get3digits("01013")
+#match("010", regionlookups$Zip.Code)
+spotfreight$FIRST_PICK_REGION <- regionlookups[match(sapply(spotfreight$FIRST_PICK_ZIP, get3digits), regionlookups$Zip.Code),2]
+summary(spotfreight$FIRST_PICK_REGION)
+spotfreight$LAST_DELIVERY_REGION <- regionlookups[match(sapply(spotfreight$LAST_DELIVERY_ZIP, get3digits), regionlookups$Zip.Code),2]
+summary(spotfreight$LAST_DELIVERY_REGION)
+spotfreight_Missing_REGION_ZIPS <- spotfreight[is.na(spotfreight$FIRST_PICK_REGION) | is.na(spotfreight$LAST_DELIVERY_REGION),] #1958 obss.
+
+
 ########## splitting the date&time columns to day,month,year cols ##########
 # reference: http://www.cyclismo.org/tutorial/R/time.html
 # 2 different types specific to time data fields in R. the POSIXct and POSIXlt 
 # POSIXct : this data type is the number of seconds since the start of January 1, 1970. Negative numbers represent the number of seconds before this time, and positive numbers represent the number of seconds afterwards
 # POSIXlt : this data type is a vector, and the entries in the vector have different meanings for day, month, year, week, etc... of that date
 # strftime : this function is used to take a time data type and convert it to a string. 
-
-#zz <- as.data.frame(cbind(year = year(px), month = month(px), day = day(px)))
-#sum(is.na(zz))
-
-#px1 <- as.POSIXlt("1/13/2016 0:00", format="%m/%d/%Y %H:%M")
-#zz1 <- as.data.frame(cbind(year = year(px1), month = month(px1), day = day(px1)))
-#px1
-#head(px1)
-#head(spotfreight$FIRST_PICK_EARLY_APPT)
 
 spotfreight$FIRST_PICK_EARLY_APPT <- as.POSIXlt(spotfreight$FIRST_PICK_EARLY_APPT, format="%m/%d/%Y %H:%M", tz="GMT")
 spotfreight$FIRST_PICK_LATE_APPT <- as.POSIXlt(spotfreight$FIRST_PICK_LATE_APPT, format="%m/%d/%Y %H:%M", tz="GMT")
@@ -185,8 +208,8 @@ CREATED_DATE_MONTH = month(spotfreight$CREATED_DATE)
 CREATED_DATE_DAY = day(spotfreight$CREATED_DATE)
 
 spotfreight <- spotfreight[, !(colnames(spotfreight) %in% c("FIRST_PICK_EARLY_APPT","FIRST_PICK_LATE_APPT","LAST_DELIVERY_EARLY_APPT", "LAST_DELIVERY_LATE_APPT", "CREATED_DATE"))]
-ncol(spotfreight)
-colnames(spotfreight)
+#ncol(spotfreight)
+#colnames(spotfreight)
 
 spotfreight <- cbind(spotfreight, 
                       PICK_EARLY_APPT_YEAR, PICK_EARLY_APPT_MONTH, PICK_EARLY_APPT_DAY,
@@ -194,8 +217,8 @@ spotfreight <- cbind(spotfreight,
                       DELIVERY_EARLY_APPT_YEAR, DELIVERY_EARLY_APPT_MONTH, DELIVERY_EARLY_APPT_DAY,
                       DELIVERY_LATE_APPT_YEAR, DELIVERY_LATE_APPT_MONTH, DELIVERY_LATE_APPT_DAY,
                       CREATED_DATE_YEAR, CREATED_DATE_MONTH, CREATED_DATE_DAY)
-ncol(spotfreight)
-colnames(spotfreight)
+#ncol(spotfreight)
+#colnames(spotfreight)
 
 # remove the temporarily created vectors
 rm(list=c("PICK_EARLY_APPT_YEAR", "PICK_EARLY_APPT_MONTH", "PICK_EARLY_APPT_DAY",
@@ -203,6 +226,62 @@ rm(list=c("PICK_EARLY_APPT_YEAR", "PICK_EARLY_APPT_MONTH", "PICK_EARLY_APPT_DAY"
           "DELIVERY_EARLY_APPT_YEAR", "DELIVERY_EARLY_APPT_MONTH", "DELIVERY_EARLY_APPT_DAY",
           "DELIVERY_LATE_APPT_YEAR", "DELIVERY_LATE_APPT_MONTH", "DELIVERY_LATE_APPT_DAY",
           "CREATED_DATE_YEAR", "CREATED_DATE_MONTH", "CREATED_DATE_DAY"))
+
+spotfreight <- na.omit(spotfreight) 
+summary(complete.cases(spotfreight)) # 24100 obs.
+
+cat_cols <- c("PICK_EARLY_APPT_YEAR", "PICK_EARLY_APPT_MONTH", "PICK_EARLY_APPT_DAY",
+              "PICK_LATE_APPT_YEAR", "PICK_LATE_APPT_MONTH", "PICK_LATE_APPT_DAY",
+              "DELIVERY_EARLY_APPT_YEAR", "DELIVERY_EARLY_APPT_MONTH", "DELIVERY_EARLY_APPT_DAY",
+              "DELIVERY_LATE_APPT_YEAR", "DELIVERY_LATE_APPT_MONTH", "DELIVERY_LATE_APPT_DAY")
+spotfreight[cat_cols] <- lapply(spotfreight[cat_cols], factor)
+str(spotfreight)
+
+########## checks how many duplicate records with the same order number ##########
+dfOrders <- as.data.frame(plyr::count(spotfreight$ORDER_NBR)) # creates a data frame with order number and corresponding frequency (repeation count)
+#colnames(dfOrders)
+dfOrders <- arrange(dfOrders, desc(freq)) # sorts the newly created data frame in descending order of the frequency column
+# gives how many repeated orders exists with what repeatations in descending order of repeatations count
+arrange(plyr::count(dfOrders, "freq"), desc(freq.1))
+#     freq freq.1
+# 1     1  11522
+# 2     2  10032
+# 3     4   1012
+# 4     6    588
+# 5     3    426
+# 6     8    168
+# 7    18    162
+# 8     5     95
+# 9     7     28
+# 10    9     27
+# 11   12     24
+# 12   16     16
+
+spotfreight <- spotfreight[, !(colnames(spotfreight) %in% c("ORDER_NBR","FIRST_PICK_ZIP","LAST_DELIVERY_ZIP","CREATED_DATE_YEAR", "CREATED_DATE_MONTH", "CREATED_DATE_DAY"))]
+str(spotfreight)
+################################################## Data slicing for train & test ############################################################
+dataForModel = spotfreight
+rows=seq(1,nrow(dataForModel),1)
+set.seed(123)
+trainRows=sample(rows,(70*nrow(dataForModel))/100)
+train = dataForModel[trainRows,] 
+test = dataForModel[-trainRows,]
+
+#Input all attributes into model 
+spotfreight_lg <- lm(ORDER_COST ~ ., data=train)
+summary(spotfreight_lg)
+
+# Error metrics evaluation on train data and test data
+#install.packages("DMwR")
+library(DMwR)
+#Error verification on train data
+regr.eval(train$ORDER_COST, spotfreight_lg$fitted.values) 
+#Error verification on test data
+Pred<-predict(spotfreight_lg,test)
+#Error in model.frame.default(Terms, newdata, na.action = na.action, xlev = object$xlevels) : 
+#  factor FIRST_PICK_MARKET has new levels LA_SHR, NC_WIL
+#regr.eval(test$ORDER_COST, Pred)
+
 
 ################################################## References ############################################################
 # Error :  "Error in n(): function should not be called directly"
@@ -213,7 +292,8 @@ rm(list=c("PICK_EARLY_APPT_YEAR", "PICK_EARLY_APPT_MONTH", "PICK_EARLY_APPT_DAY"
 # which are masked due to conflicts due to same function name from more than 1 loaded library for the running environment
 # Ref   : http://stackoverflow.com/questions/22801153/dplyr-error-in-n-function-should-not-be-called-directly
 
-
+# length of string : http://stackoverflow.com/questions/11134812/how-to-find-the-length-of-a-string-in-r
+#
 
 ################################################## Rough code ############################################################
 #which(is.na(as.numeric(as.character(spotfreight$FIRST_PICK_ZIP))))
@@ -226,3 +306,10 @@ rm(list=c("PICK_EARLY_APPT_YEAR", "PICK_EARLY_APPT_MONTH", "PICK_EARLY_APPT_DAY"
 #!is.na(as.numeric(spotfreight$FIRST_PICK_ZIP))
 #check.numeric <- function(N){ !length(grep("[^[:digit:]]", as.character(N)))}
 #check.numeric("0")
+#zz <- as.data.frame(cbind(year = year(px), month = month(px), day = day(px)))
+#sum(is.na(zz))
+#px1 <- as.POSIXlt("1/13/2016 0:00", format="%m/%d/%Y %H:%M")
+#zz1 <- as.data.frame(cbind(year = year(px1), month = month(px1), day = day(px1)))
+#px1
+#head(px1)
+#head(spotfreight$FIRST_PICK_EARLY_APPT)
