f414bdb82f43c8d16eb35a7775b4254cd85f5416
srikant2016 <atmakuri.srikant@gmail.com>
2016-12-29 03:20:48 -0800

    Proprocessing - in progress

--- FM_AtmakuriSrikant.R
+++ FM_AtmakuriSrikant.R
@@ -4,15 +4,15 @@ rm(list=ls(all=TRUE))
 #setwd("C:\\Srikant\\INSOFE\\Projects\\Freight Management\\Dataset_QuickFreight")
 setwd("D:\\github_repo\\INSOFE_CPEE_Project_freightmgmt")
 
-# Reads all Data
+################################################## Reads all Data & basic understanding of it ############################################################
 spotfreight <- read.csv("spotfreightdata.csv",header=T, na.strings = c("","NA"," "))    # 37699 obs
 regionlookups <- read.csv("regionlookups.csv",header=T, na.strings = c("","NA"," "))    # 906 obs
 marketzips <- read.csv("marketzips.csv",header=T, na.strings = c("","NA"," "))          # 905 obs
 equipmentcodes <- read.csv("equipmentcodes.csv",header=T, na.strings = c("","NA"," "))  # 292 obs
 
+library(plyr)
 library(dplyr)
 library(lubridate)
-library(plyr)
 
 # View(spotfreight)
 # View(regionlookups)
@@ -32,7 +32,18 @@ summary(marketzips)
 str(equipmentcodes)
 summary(equipmentcodes)
 
-# Preprocessing the data
+
+########## correlation chart ##########
+#library(corrplot) 
+#spotfreight_cor <- cor(spotfreight[,3:5])
+#spotfreight_cor
+#corrplot(spotfreight_cor)
+
+################################################## Preprocessing the data ############################################################
+# One approach to remove NA or empty cells either assign NA at the time of reading the file and using na.omit() to remove all of NA
+# Other option to use the below approach to remove after reading the file as-is
+# removes both na and empty values in any columns of spotfreight. 
+#spotfreight <- spotfreight[!apply(is.na(spotfreight) | spotfreight == "", 1, all),]
 sum(is.na(spotfreight)) # checks for any rows with NA in the spotfreight data = 5620 na's 
 spotfreight <- na.omit(spotfreight) # removes all the rows with NA in one or more columns =  34693 records 
 summary(complete.cases(spotfreight)) # checks if any of the rows has NAs in any of its columns of the spotfreight data
@@ -58,77 +69,12 @@ cat("Unique orders count : " , n_distinct(spotfreight$ORDER_NBR)) # 23512
 # 169459,"V",716,9420,1500,"47909","1/21/2016 0:00","1/22/2016 0:00","45011","1/25/2016 0:00","1/25/2016 0:00","N","1/16/2016 0:00"
 # 169459,"V",716,9420,1500,"40330","1/21/2016 0:00","1/21/2016 0:00","45011","1/25/2016 0:00","1/25/2016 0:00","N","1/16/2016 0:00"
 
-# remove duplicate records (ignoring the "CREATED_DATE" field)
-spotfreight_unq <- spotfreight[!duplicated(spotfreight[,!(colnames(spotfreight) %in% c("CREATED_DATE"))]), ]
-
-# removes space and - characters from both FIRST_PICK_ZIP & LAST_DELIVERY_ZIP columns
-spotfreight$FIRST_PICK_ZIP <- gsub("[ -]", "", spotfreight$FIRST_PICK_ZIP) 
-spotfreight$LAST_DELIVERY_ZIP <- gsub("[ -]", "", spotfreight$LAST_DELIVERY_ZIP)
-
-# remove records with alphanumeric zip codes either in FIRST_PICK_ZIP or LAST_DELIVERY_ZIP or both
-check.numeric <- function(N){ !length(grep("[^[:digit:]]", as.character(N)))}
-spotfreight <- spotfreight[sapply(spotfreight$FIRST_PICK_ZIP, check.numeric) & sapply(spotfreight$LAST_DELIVERY_ZIP, check.numeric),] 
-#spotfreight$VALID_PICK_ZIP <- sapply(spotfreight$FIRST_PICK_ZIP, check.numeric)
-#spotfreight$VALID_PICK_ZIP <- NULL
-#spotfreight$VALID_DELIVERY_ZIP <- sapply(spotfreight$LAST_DELIVERY_ZIP, check.numeric)
-#spotfreight$VALID_DELIVERY_ZIP <- NULL
-
-# check & remove records with zero values in ORDER_COST field (the target variable)
-summary(spotfreight$ORDER_COST)
-arrange(summarise(group_by(spotfreight, ORDER_COST), orders = n()), desc(-ORDER_COST)) # there are around 6326 records with zero ORDER_COST (the target variable)
-n_distinct(spotfreight) # 34273
-spotfreight <- spotfreight[!((spotfreight$ORDER_COST == 0) | (spotfreight$ORDER_COST == 0.01)),] 
-n_distinct(spotfreight) # 27946 (6327 records with  0 ORDER_COST excluded )
-
-# check & remove records with zero or negative WEIGHT
-summary(spotfreight$WEIGHT)
-arrange(summarise(group_by(spotfreight, WEIGHT), orders = n()), desc(-WEIGHT)) # there are around 1681 records with zero WEIGHT (to be excluded)
-spotfreight <- spotfreight[!(spotfreight$WEIGHT == 0),] 
-n_distinct(spotfreight) # 26265 (1681 records with 0 WEIGHT excluded )
-
-# check & remove records with zero or negative CUSTOMER_MILES
-summary(spotfreight$CUSTOMER_MILES)
-arrange(summarise(group_by(spotfreight, CUSTOMER_MILES), orders = n()), desc(-CUSTOMER_MILES)) # there are around 1681 records with zero CUSTOMER_MILES (to be excluded)
-spotfreight <- spotfreight[!(spotfreight$CUSTOMER_MILES == 0),] 
-n_distinct(spotfreight) # 26166 (99 records with 0 CUSTOMER_MILES excluded )
-
-# check & understand equipments types and add 2 new columns based on the values in equipmentcodes dataframe and equipment_type value in spotfreight data frame
-summary(spotfreight$EQUIPMENT_TYPE) # this is to understand how many different types of equipments are avaialable in the given raw  data
-equipments <- arrange(summarise(group_by(spotfreight, EQUIPMENT_TYPE), orders = n()), desc(orders)) # this is to understand how orders with what equipment type code in the raw data
-spotfreight$Equipment_New_Abbr <- equipmentcodes[match(spotfreight$EQUIPMENT_TYPE, equipmentcodes$EQUIPMENT_TYPE),2]
-names(spotfreight)[names(spotfreight) == 'Equipment_New_Abbr'] <- 'EQUIPMENT_NEW_ABBR' # renames a column name from old to a new name
-summary(spotfreight$EQUIPMENT_NEW_ABBR)
-spotfreight$EQUIPMENT_TYPE_DESC <- equipmentcodes[match(spotfreight$EQUIPMENT_TYPE, equipmentcodes$EQUIPMENT_TYPE),3]
-summary(spotfreight$EQUIPMENT_TYPE_DESC)
-
-spotfreight_Missing_EQUIPMENT_TYPE <- spotfreight[is.na(spotfreight$EQUIPMENT_NEW_ABBR) | is.na(spotfreight$EQUIPMENT_TYPE_DESC),]
-spotfreight <- na.omit(spotfreight) # 95records removed for which EQUIPMENT_ABBR / EQUIPMENT_TYPE_DESC isn't found from equipmentcodes dataframe based on the eequipment type code in spotfreight data frame
-summary(complete.cases(spotfreight)) # 26071 obs.
-
-# check & remove records with zero or negative CUSTOMER_MILES
-summary(spotfreight$CUSTOMER_MILES)
-arrange(summarise(group_by(spotfreight, CUSTOMER_MILES), orders = n()), desc(-CUSTOMER_MILES)) # there are around 1681 records with zero CUSTOMER_MILES (to be excluded)
-spotfreight <- spotfreight[!(spotfreight$CUSTOMER_MILES == 0),] 
-n_distinct(spotfreight) # 26166 (99 records with 0 CUSTOMER_MILES excluded )
-
-
-#which(is.na(as.numeric(as.character(spotfreight$FIRST_PICK_ZIP))))
-#sum(is.na(as.numeric(as.character(spotfreight$FIRST_PICK_ZIP))))
-#sapply(spotfreight$FIRST_PICK_ZIP, length(gsub("[:digit:]", "", unique(spotfreight$FIRST_PICK_ZIP))
-#gsub("\\D+", "", "V2S7W6",ignore.case = TRUE)
-#gsub("[:digit:]", "", "67119",ignore.case = TRUE)
-#gsub("[ -]", "", " N5W 6C7")
-#spotfreight$FIRST_PICK_ZIP<-gsub("[\\S+-]", "", spotfreight$FIRST_PICK_ZIP)
-#!is.na(as.numeric(spotfreight$FIRST_PICK_ZIP))
-#check.numeric <- function(N){ !length(grep("[^[:digit:]]", as.character(N)))}
-#check.numeric("0")
-
-
-dfOrders <- as.data.frame(count(spotfreight$ORDER_NBR)) # creates a data frame with order number and corresponding frequency (repeation count)
+########## checks how many duplicate records with the same order number ##########
+dfOrders <- as.data.frame(plyr::count(spotfreight$ORDER_NBR)) # creates a data frame with order number and corresponding frequency (repeation count)
 #colnames(dfOrders)
 dfOrders <- arrange(dfOrders, desc(freq)) # sorts the newly created data frame in descending order of the frequency column
 # gives how many repeated orders exists with what repeatations in descending order of repeatations count
-arrange(count(dfOrders, "freq"), desc(freq.1))
+arrange(plyr::count(dfOrders, "freq"), desc(freq.1))
 # freq freq.1
 # 1     1  15482
 # 2     2  13702
@@ -147,18 +93,61 @@ arrange(count(dfOrders, "freq"), desc(freq.1))
 # 15   15     15
 # 16   14     14
 
+########## remove duplicate records (ignoring the "CREATED_DATE" field) ##########
+spotfreight_unq <- spotfreight[!duplicated(spotfreight[,!(colnames(spotfreight) %in% c("CREATED_DATE"))]), ]
 
-# One approach to remove NA or empty cells either assign NA at the time of reading the file and using na.omit() to remove all of NA
-# Other option to use the below approach to remove after reading the file as-is
-# removes both na and empty values in any columns of spotfreight. 
-#spotfreight <- spotfreight[!apply(is.na(spotfreight) | spotfreight == "", 1, all),]
-#summary(spotfreight)
+########## removes space and - characters from both FIRST_PICK_ZIP & LAST_DELIVERY_ZIP columns ##########
+spotfreight$FIRST_PICK_ZIP <- gsub("[ -]", "", spotfreight$FIRST_PICK_ZIP) 
+spotfreight$LAST_DELIVERY_ZIP <- gsub("[ -]", "", spotfreight$LAST_DELIVERY_ZIP)
 
-#library(corrplot) # used for rendering the correlation chart
-#spotfreight_cor <- cor(spotfreight[,3:5])
-#spotfreight_cor
-#corrplot(spotfreight_cor)
+########## remove records with alphanumeric zip codes either in FIRST_PICK_ZIP or LAST_DELIVERY_ZIP or both ##########
+check.numeric <- function(N){ !length(grep("[^[:digit:]]", as.character(N)))}
+spotfreight <- spotfreight[sapply(spotfreight$FIRST_PICK_ZIP, check.numeric) & sapply(spotfreight$LAST_DELIVERY_ZIP, check.numeric),] 
+#spotfreight$VALID_PICK_ZIP <- sapply(spotfreight$FIRST_PICK_ZIP, check.numeric)
+#spotfreight$VALID_PICK_ZIP <- NULL
+#spotfreight$VALID_DELIVERY_ZIP <- sapply(spotfreight$LAST_DELIVERY_ZIP, check.numeric)
+#spotfreight$VALID_DELIVERY_ZIP <- NULL
 
+########## check & remove records with zero values in ORDER_COST field (the target variable) ##########
+summary(spotfreight$ORDER_COST)
+arrange(summarise(group_by(spotfreight, ORDER_COST), orders = n()), desc(-ORDER_COST)) # there are around 6326 records with zero ORDER_COST (the target variable)
+n_distinct(spotfreight) # 34273
+spotfreight <- spotfreight[!((spotfreight$ORDER_COST == 0) | (spotfreight$ORDER_COST == 0.01)),] 
+n_distinct(spotfreight) # 27946 (6327 records with  0 ORDER_COST excluded )
+
+########## check & remove records with zero or negative WEIGHT ##########
+summary(spotfreight$WEIGHT)
+arrange(summarise(group_by(spotfreight, WEIGHT), orders = n()), desc(-WEIGHT)) # there are around 1681 records with zero WEIGHT (to be excluded)
+spotfreight <- spotfreight[!(spotfreight$WEIGHT == 0),] 
+n_distinct(spotfreight) # 26265 (1681 records with 0 WEIGHT excluded )
+
+########## check & remove records with zero or negative CUSTOMER_MILES ##########
+summary(spotfreight$CUSTOMER_MILES)
+arrange(summarise(group_by(spotfreight, CUSTOMER_MILES), orders = n()), desc(-CUSTOMER_MILES)) # there are around 1681 records with zero CUSTOMER_MILES (to be excluded)
+spotfreight <- spotfreight[!(spotfreight$CUSTOMER_MILES == 0),] 
+n_distinct(spotfreight) # 26166 (99 records with 0 CUSTOMER_MILES excluded )
+
+########## check & understand equipments types ##########
+#and add 2 new columns based on the values in equipmentcodes dataframe and equipment_type value in spotfreight data frame
+summary(spotfreight$EQUIPMENT_TYPE) # this is to understand how many different types of equipments are avaialable in the given raw  data
+equipments <- arrange(summarise(group_by(spotfreight, EQUIPMENT_TYPE), orders = n()), desc(orders)) # this is to understand how orders with what equipment type code in the raw data
+spotfreight$Equipment_New_Abbr <- equipmentcodes[match(spotfreight$EQUIPMENT_TYPE, equipmentcodes$EQUIPMENT_TYPE),2]
+names(spotfreight)[names(spotfreight) == 'Equipment_New_Abbr'] <- 'EQUIPMENT_NEW_ABBR' # renames a column name from old to a new name
+summary(spotfreight$EQUIPMENT_NEW_ABBR)
+spotfreight$EQUIPMENT_TYPE_DESC <- equipmentcodes[match(spotfreight$EQUIPMENT_TYPE, equipmentcodes$EQUIPMENT_TYPE),3]
+summary(spotfreight$EQUIPMENT_TYPE_DESC)
+
+spotfreight_Missing_EQUIPMENT_TYPE <- spotfreight[is.na(spotfreight$EQUIPMENT_NEW_ABBR) | is.na(spotfreight$EQUIPMENT_TYPE_DESC),]
+spotfreight <- na.omit(spotfreight) # 95records removed for which EQUIPMENT_ABBR / EQUIPMENT_TYPE_DESC isn't found from equipmentcodes dataframe based on the eequipment type code in spotfreight data frame
+summary(complete.cases(spotfreight)) # 26071 obs.
+
+
+########## splitting the date&time columns to day,month,year cols ##########
+# reference: http://www.cyclismo.org/tutorial/R/time.html
+# 2 different types specific to time data fields in R. the POSIXct and POSIXlt 
+# POSIXct : this data type is the number of seconds since the start of January 1, 1970. Negative numbers represent the number of seconds before this time, and positive numbers represent the number of seconds afterwards
+# POSIXlt : this data type is a vector, and the entries in the vector have different meanings for day, month, year, week, etc... of that date
+# strftime : this function is used to take a time data type and convert it to a string. 
 
 #zz <- as.data.frame(cbind(year = year(px), month = month(px), day = day(px)))
 #sum(is.na(zz))
@@ -168,14 +157,6 @@ arrange(count(dfOrders, "freq"), desc(freq.1))
 #px1
 #head(px1)
 #head(spotfreight$FIRST_PICK_EARLY_APPT)
-
-
-# splitting the date&time cols to day,month,year cols
-# reference: http://www.cyclismo.org/tutorial/R/time.html
-# 2 different types specific to time data fields in R. the POSIXct and POSIXlt 
-# POSIXct : this data type is the number of seconds since the start of January 1, 1970. Negative numbers represent the number of seconds before this time, and positive numbers represent the number of seconds afterwards
-# POSIXlt : this data type is a vector, and the entries in the vector have different meanings for day, month, year, week, etc... of that date
-# strftime : this function is used to take a time data type and convert it to a string. 
 
 spotfreight$FIRST_PICK_EARLY_APPT <- as.POSIXlt(spotfreight$FIRST_PICK_EARLY_APPT, format="%m/%d/%Y %H:%M", tz="GMT")
 spotfreight$FIRST_PICK_LATE_APPT <- as.POSIXlt(spotfreight$FIRST_PICK_LATE_APPT, format="%m/%d/%Y %H:%M", tz="GMT")
@@ -187,10 +168,34 @@ spotfreight1 <- spotfreight[, !(colnames(spotfreight) %in% c("FIRST_PICK_EARLY_A
 colnames(spotfreight1)
 
 
-spotfreight1 <- as.data.frame(cbind(spotfreight1, 
-                                    PICK_EARLY_APPT_YEAR = year(px1), PICK_EARLY_APPT_MONTH = month(px1), PICK_EARLY_APPT_DAY = day(px1),
-                                    PICK_EARLY_APPT_YEAR = year(px2), PICK_EARLY_APPT_MONTH = month(px2), PICK_EARLY_APPT_DAY = day(px2),
-                                    PICK_EARLY_APPT_YEAR = year(px3), PICK_EARLY_APPT_MONTH = month(px3), PICK_EARLY_APPT_DAY = day(px3),
-                                    PICK_EARLY_APPT_YEAR = year(px4), PICK_EARLY_APPT_MONTH = month(px4), PICK_EARLY_APPT_DAY = day(px4),
-                                    PICK_EARLY_APPT_YEAR = year(px5), PICK_EARLY_APPT_MONTH = month(px5), PICK_EARLY_APPT_DAY = day(px5)
-                                    ))
+spotfreight1 <- within(spotfreight, as.data.frame(cbind(spotfreight1, 
+                                    PICK_EARLY_APPT_YEAR = year(FIRST_PICK_EARLY_APPT), PICK_EARLY_APPT_MONTH = month(FIRST_PICK_EARLY_APPT), PICK_EARLY_APPT_DAY = day(FIRST_PICK_EARLY_APPT),
+                                    PICK_LATE_APPT_YEAR = year(FIRST_PICK_LATE_APPT), PICK_LATE_APPT_MONTH = month(FIRST_PICK_LATE_APPT), PICK_LATE_APPT_DAY = day(FIRST_PICK_LATE_APPT),
+                                    DELIVERY_EARLY_APPT_YEAR = year(LAST_DELIVERY_EARLY_APPT), DELIVERY_EARLY_APPT_MONTH = month(LAST_DELIVERY_EARLY_APPT), DELIVERY_EARLY_APPT_DAY = day(LAST_DELIVERY_EARLY_APPT),
+                                    DELIVERY_LATE_APPT_YEAR = year(LAST_DELIVERY_LATE_APPT), DELIVERY_LATE_APPT_MONTH = month(LAST_DELIVERY_LATE_APPT), DELIVERY_LATE_APPT_DAY = day(LAST_DELIVERY_LATE_APPT),
+                                    CREATED_DATE_YEAR = year(CREATED_DATE), CREATED_DATE_MONTH = month(CREATED_DATE), CREATED_DATE_DAY = day(CREATED_DATE)
+                                    )))
+
+
+################################################## References ############################################################
+# Error :  "Error in n(): function should not be called directly"
+# Error : Error in UseMethod("group_by_") : no applicable method for 'group_by_' applied to an object of class "c('integer', 'numeric')"
+# Soln  : Sometimes this occurs due to conflicting function due to same function available from more than on loaded library. to avoid such situation
+# use explicit function calling method (i.e. prefix the function name with <library>::<function>)
+# to check conflicting functions at any given point of time in a running environment use "conflicts()" function. It would show all the functions
+# which are masked due to conflicts due to same function name from more than 1 loaded library for the running environment
+# Ref   : http://stackoverflow.com/questions/22801153/dplyr-error-in-n-function-should-not-be-called-directly
+
+
+
+################################################## Rough code ############################################################
+#which(is.na(as.numeric(as.character(spotfreight$FIRST_PICK_ZIP))))
+#sum(is.na(as.numeric(as.character(spotfreight$FIRST_PICK_ZIP))))
+#sapply(spotfreight$FIRST_PICK_ZIP, length(gsub("[:digit:]", "", unique(spotfreight$FIRST_PICK_ZIP))
+#gsub("\\D+", "", "V2S7W6",ignore.case = TRUE)
+#gsub("[:digit:]", "", "67119",ignore.case = TRUE)
+#gsub("[ -]", "", " N5W 6C7")
+#spotfreight$FIRST_PICK_ZIP<-gsub("[\\S+-]", "", spotfreight$FIRST_PICK_ZIP)
+#!is.na(as.numeric(spotfreight$FIRST_PICK_ZIP))
+#check.numeric <- function(N){ !length(grep("[^[:digit:]]", as.character(N)))}
+#check.numeric("0")
